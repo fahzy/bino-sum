35041c8c40bbf816ff72ac6aafeeca9b5e43e2d0
fahzy <kabeloleiee@gmail.com>
2022-07-17 18:09:13 +0200

    Started with function_F

--- bino-sum-code.py
+++ bino-sum-code.py
@@ -1,7 +1,15 @@
 import argparse
 
-def function_F(N: int, K: int):
-    pass
+def function_F(N: int, P: int):
+    if P == 0: return 1
+
+    subsets: list = []
+
+    for x in range(1, N+1):
+        for y in range(1, P+1):
+            pass
+        
+    
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--textfile", help="The name of the textfile to be read.")
@@ -22,22 +30,28 @@ if args.textfile:
 
     sum: int = 0
     DENOMINATOR: int = 1000000007
-    T, N, P = (0, 0, 0)
+    N, P = (0, 0)
     line: str = ""
 
     for x in range(test_cases):
         line = str(f.readline()).strip()
 
         broken: str = line.split(" ")
-        T = int(broken[0])
+        N = int(broken[0])
         P = int(broken[1])
 
-        if T < 1 or T > 1000:
-            print("T cannot be less than 1 nor can it be greater than 1000")
-            continue
+        # print(line)
+        # print(T)
+        # print(N)
+
         if N < 1 or N > 1000:
-            print("T cannot be less than 1 nor can it be greater than 1000")
+            print("N cannot be less than 1 nor can it be greater than 1000")
             continue
+        if P < 0 or P > 1000:
+            print("P cannot be less than 0 nor can it be greater than 1000")
+            continue
+
+
 
         
 
